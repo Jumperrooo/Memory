f5c70dd05241969fdc5402c20e2c03141b16602e
Pawel Nawrot <pakajumper@gmail.com>
2023-06-10 15:33:26 +0200

    trubleshooting problems

--- memory.js
+++ memory.js
@@ -11,45 +11,68 @@ cards.forEach(function(card,index){
 var oneVisible= false;
 var turnCounter=0;
 var indexOfPreviusCard;
+var lock=false; 
 
 function revealCard(nr)
 {
-var obraz = "url(img/"+ arrayOfCards[nr] + ")";
+    var opacityValue= $('#card'+nr).css('opacity');
 
-$('#card'+nr).css('background-image', obraz);
-$('#card'+nr).addClass('activeCard');
-$('#card'+nr).removeClass('card');
+    if(opacityValue!=0 && lock==false)
+    { 
+        lock=true;
+        var obraz = "url(img/"+ arrayOfCards[nr] + ")";
 
-
-if(!oneVisible)
-{
-    //first card of pair
-oneVisible=true;
-indexOfPreviusCard=nr;
-}
-else
-{
-    //second card of pair
-    if(arrayOfCards[indexOfPreviusCard]==arrayOfCards[nr])
-    {
-        setTimeout(function()
-        {hideBothCards(nr,indexOfPreviusCard)},750);
+        $('#card'+nr).css('background-image', obraz);
+        $('#card'+nr).addClass('activeCard');
+        $('#card'+nr).removeClass('card');
+        
+        
+        if(!oneVisible)
+        {
+            //first card of pair
+        oneVisible=true;
+        indexOfPreviusCard=nr;
+        lock=false;
+        }else{
+            //second card of pair
+            if(arrayOfCards[indexOfPreviusCard]==arrayOfCards[nr])
+            {
+                if(indexOfPreviusCard!==nr){
+                turnCounter++; 
+                setTimeout(function(){
+                    hideBothCards(nr,indexOfPreviusCard)
+                },750);
+                cards[indexOfPreviusCard].removeEventListener("click",revealCard);
+                cards[nr].removeEventListener("click", revealCard);
+                }
+            }
+            else
+            {
+                turnCounter++;
+                setTimeout(function(){
+                    restoreBothCards(nr,indexOfPreviusCard)
+                },750);
+            }
+        
+            $('.score').html("Which Turn: "+turnCounter);
+            oneVisible=false;
+        }
     }
-    else
-    {
-
-    }
-
-    turnCounter++;
-    $('.score').html("Which Turn: "+turnCounter);
-    oneVisible=false;
-}
- 
 }
 
 function hideBothCards(nr1, nr2)
 {
     $('#card'+nr1).css('opacity','0');
     $('#card'+nr2).css('opacity','0');
+    lock=false;
 }
-
+function restoreBothCards(nr1, nr2)
+{
+    $('#card'+nr1).css('background-image', 'url(img/card.png)');
+    $('#card'+nr1).addClass('card');
+    $('#card'+nr1).removeClass('activeCard');
+    
+    $('#card'+nr2).css('background-image', 'url(img/card.png)');
+    $('#card'+nr2).addClass('card');
+    $('#card'+nr2).removeClass('activeCard');
+}
